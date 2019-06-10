ba0e41c8add3a1d3e50f0c6a1a80245e5890195b
cclauwers <cclauwers@gmail.com>
2019-06-10 21:34:43 +0200

    apartat B

--- src/Machine.java
+++ src/Machine.java
@@ -1,7 +1,24 @@
 public class Machine extends MachineComponent {
 
+    private boolean broken = false;
     @Override
     public boolean isBroken() {
         return broken;
+    }
+
+    public void setBroken() {
+        broken = true;
+        if (broken) {
+            setChanged();
+            notifyObservers();
+        }
+    }
+
+    public void repair() {
+        broken = false;
+        if (!broken) {
+            setChanged();
+            notifyObservers();
+        }
     }
 }
