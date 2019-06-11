78171b1456ff9b50e77923cb8574d910e987a1bc
cclauwers <cclauwers@gmail.com>
2019-06-11 10:45:49 +0200

    modificacio  addcomponent machinecomposite

--- src/MachineComposite.java
+++ src/MachineComposite.java
@@ -1,12 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 
-public class MachineComposite extends MachineComponent{
+public class MachineComposite extends MachineComponent implements Observer {
     private List<MachineComponent> components = new ArrayList<>();
+    public  List<MachineComponent> componetsBroken = new ArrayList<>();
 
     public void addComponent(MachineComponent mc) {
         components.add(mc);
+        mc.addObserver(this);
+        if(mc.isBroken()) {
+            this.componetsBroken.add(mc);
+            if (!this.broken && this.componetsBroken.size() == 1) {
+                this.notifyObservers();
+            }
+        }
      }
 
     @Override
@@ -20,5 +30,10 @@ public class MachineComposite extends MachineComponent{
             }
         }
         return false;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
     }
 }
